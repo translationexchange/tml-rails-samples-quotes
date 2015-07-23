365684a27884c548866257781311d9ecf0746337
Michael Berkovich <theiceberk@gmail.com>
2015-07-22 19:43:50 -0700

    point TML to production

--- config/initializers/tml.rb
+++ config/initializers/tml.rb
@@ -1,8 +1,13 @@
 Tml.configure do |config|
+
   config.application = {
-      :host => 'http://localhost:3000',
-      :token => '46a15acfc4987368a097e12cc883d8e76bd35fa97c5ebe8bcba7739927e0da37'
+      :token => '856f9db4ed172e45f7b6d377aa30b4583a358b6ede517e51e514eb176152c28c'
   }
+
+  # config.application = {
+  #     :host => 'http://localhost:3000',
+  #     :token => '46a15acfc4987368a097e12cc883d8e76bd35fa97c5ebe8bcba7739927e0da37'
+  # }
 
   config.cache = {
       :enabled      => false,
@@ -12,8 +17,9 @@ Tml.configure do |config|
   }
 
   config.logger  = {
-      :enabled  => true,
+      :enabled  => false,
       :path     => "#{Rails.root}/log/tml.log",
       :level    => 'debug'
   }
+
 end
