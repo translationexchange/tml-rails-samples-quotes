7329cfdcacee3b569935f7dbdd23b675206f20a2
Michael Berkovich <theiceberk@gmail.com>
2015-07-23 20:11:27 -0700

    better integration

--- config/initializers/tml.rb
+++ config/initializers/tml.rb
@@ -1,20 +1,29 @@
 Tml.configure do |config|
 
-  config.application = {
-      :token => '856f9db4ed172e45f7b6d377aa30b4583a358b6ede517e51e514eb176152c28c'
-  }
+  if Rails.env.production?
+    config.application = {
+        :token => '856f9db4ed172e45f7b6d377aa30b4583a358b6ede517e51e514eb176152c28c'
+    }
 
-  # config.application = {
-  #     :host => 'http://localhost:3000',
-  #     :token => '46a15acfc4987368a097e12cc883d8e76bd35fa97c5ebe8bcba7739927e0da37'
-  # }
+    config.cache = {
+        :enabled      => false,
+        :adapter      => 'memcache',
+        :host         => 'tememcached.yptuob.cfg.usw1.cache.amazonaws.com:11211',
+        :namespace    => '856f9db4ed172e45f7b6d377aa30b4583a358b6ede517e51e514eb176152c28c'
+    }
+  else
+    config.application = {
+        :host => 'http://localhost:3000',
+        :token => '46a15acfc4987368a097e12cc883d8e76bd35fa97c5ebe8bcba7739927e0da37'
+    }
 
-  config.cache = {
-      :enabled      => false,
-      :adapter      => 'memcache',
-      :host         => 'localhost:11211',
-      :namespace    => 'quotes'
-  }
+    config.cache = {
+        :enabled      => false,
+        :adapter      => 'memcache',
+        :host         => 'localhost:11211',
+        :namespace    => '46a15acfc4987368a097e12cc883d8e76bd35fa97c5ebe8bcba7739927e0da37'
+    }
+  end
 
   config.logger  = {
       :enabled  => false,
